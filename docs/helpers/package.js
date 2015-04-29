2e596b2a022ffb7d55b1ee405be6bb14510a9128
akileez <git@keeda.net>
2015-04-29 12:13:54 -0400

    clean and lint

--- docs/helpers/package.js
+++ docs/helpers/package.js
@@ -1,5 +1,4 @@
-var path   = require('path')
-var fs     = require('fs')
+var path = require('path')
 var extend = require('extend-basic')
 
 var config = require(path.resolve(process.cwd(), 'package.json'))
@@ -10,16 +9,15 @@ exports.pkg = function (key) {
   return opts[key] || ''
 }
 
-exports.author = function() {
+exports.author = function (name) {
   var author = config.author ? (config.author.name || config.author) : name
   var url = config.author.url ? '(' + config.author.url + ')' : ''
   return '[' + author + ']' + url
 }
 
 exports.copyright = function (startYear) {
-  if (typeof startYear === 'object' ){startYear = '';}
-  var author = config.author ? (config.author.name || config.author) : name
+  if (typeof startYear === 'object') startYear = ''
   var thisyear = new Date().getFullYear().toString() + ' '
   var date = startYear ? startYear + '-' + thisyear : thisyear
-  return 'Copyright (c) ' + date + author + ', contributors.'
+  return 'Copyright (c) ' + date + ' '
 }
