4da4456298692b8b4b3700b797e087ff9b52de5d
akileez <git@keeda.net>
2015-04-30 14:16:23 -0400

    test prep 2

--- docs/app/assemble-readme.js
+++ docs/app/assemble-readme.js
@@ -23,6 +23,30 @@ assemble.task('generate', function () {
   .pipe(assemble.dest('docs/test'))
 })
 
+assemble.task('generate1', function () {
+  return assemble
+  .src('docs/layouts/templates/tmpl.hbs')
+  .pipe(rename('README.md'))
+  .pipe(assemble.dest('docs/test'))
+})
+
+assemble.task('generate2', function () {
+  return assemble
+  .src('docs/layouts/templates/tmpl.hbs')
+  .pipe(extname('md'))
+  .pipe(assemble.dest('docs/test'))
+  .pipe(rename({basename: 'README'}))
+  .pipe(assemble.dest('docs/test'))
+})
+
+assemble.task('generate3', function () {
+  return assemble
+  .src('docs/layouts/templates/tmpl.hbs')
+  .pipe(extname('md'))
+  .pipe(rename('README.md'))
+  .pipe(assemble.dest('docs/test'))
+})
+
 assemble.task('rename', function () {
   return assemble
   .src('docs/test/tmpl.md')
