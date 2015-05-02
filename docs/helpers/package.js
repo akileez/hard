fd3fd04c72530e5eafb7ce59c7e252e6e3662937
akileez <git@keeda.net>
2015-05-02 01:33:55 -0400

    refactor package helper

--- docs/helpers/package.js
+++ docs/helpers/package.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var extend = require('extend-basic')
+var time = require('o-clock')
 
 var config = require(path.resolve(process.cwd(), 'package.json'))
 
@@ -11,13 +12,22 @@ exports.pkg = function (key) {
 
 exports.author = function (name) {
   var author = config.author ? (config.author.name || config.author) : name
-  var url = config.author.url ? '(' + config.author.url + ')' : ''
-  return '[' + author + ']' + url
+  return author
 }
 
-exports.copyright = function (startYear) {
+exports.copyright = function (date) {
+  if (typeof date === 'object') date = ''
+  if (typeof date === 'string') return 'Copyright (c) ' + date
+  return 'Copyright (c)'
+}
+
+exports.year = function (startYear) {
   if (typeof startYear === 'object') startYear = ''
   var thisyear = new Date().getFullYear().toString() + ' '
   var date = startYear ? startYear + '-' + thisyear : thisyear
-  return 'Copyright (c) ' + date + ' '
+  return date
+}
+
+exports.time = function () {
+  return time()
 }
