fb6e55eba1b0af99e78f9399bf21c38bea2b7db7
akileez <git@keeda.net>
2015-05-02 01:33:17 -0400

    remove test params

--- assemblefile.js
+++ assemblefile.js
@@ -23,19 +23,6 @@ require('./docs/app/assemble-support.js')  // delete files
 // Assemble Jobs
 // ///////////////////////////////////////////////////////////////////////////////
 
-// using this (default) as the workaround
 assemble.task('default', series.sync(['loadLayouts', 'generate', 'rename']))
-
-// test1 using assemble.src --> gulp-rename -> assemble.dest -- template (tmpl.hbs) does not process
-// this used to work without issue.
-assemble.task('test1', series.sync(['loadLayouts', 'generate1']))
-
-// test2 using assemble.src --> gulp-extname --> assemble.dest --> gulp-rename --> assemble.dest
-// same as default task without removing tmpl.hbs (clean:up). This works but extra step needed
-assemble.task('test2', series.sync(['loadLayouts', 'generate2']))
-
-// test3 using assemble.src --> gulp-extname --> gulp-rename ->  assemble.dest
-// this does not process the template (tmpl.hbs). Same as test1
-assemble.task('test3', series.sync(['loadLayouts', 'generate3']))
 
 assemble.task('clean', series.sync(['clean:all']))
--- docs/app/assemble-readme.js
+++ docs/app/assemble-readme.js
@@ -11,7 +11,6 @@
 
 var assemble = require('assemble')
 var extname = require('gulp-extname')
-var rename = require('gulp-rename')
 
 // Assemble Readme Generation
 // ///////////////////////////////////////////////////////////////////////////////
@@ -20,29 +19,5 @@ assemble.task('generate', function () {
   return assemble
   .src('docs/layouts/templates/tmpl.hbs')
   .pipe(extname('md'))
-  .pipe(assemble.dest('docs/test'))
-})
-
-assemble.task('generate1', function () {
-  return assemble
-  .src('docs/layouts/templates/tmpl.hbs')
-  .pipe(rename('README.md'))
-  .pipe(assemble.dest('docs/test'))
-})
-
-assemble.task('generate2', function () {
-  return assemble
-  .src('docs/layouts/templates/tmpl.hbs')
-  .pipe(extname('md'))
-  .pipe(assemble.dest('docs/test'))
-  .pipe(rename({basename: 'README'}))
-  .pipe(assemble.dest('docs/test'))
-})
-
-assemble.task('generate3', function () {
-  return assemble
-  .src('docs/layouts/templates/tmpl.hbs')
-  .pipe(extname('md'))
-  .pipe(rename('README.md'))
   .pipe(assemble.dest('docs/test'))
 })
