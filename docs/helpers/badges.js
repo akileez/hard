91edda1a8f16bc58339cc19208e1fa43368c0898
akileez <git@keeda.net>
2015-05-09 21:16:18 -0400

    badges, second run

--- docs/helpers/badges.js
+++ docs/helpers/badges.js
@@ -1,7 +1,20 @@
 
 
+exports.badgeShields = function (subject, status) {
+  var link = '[![' + status + '][' + subject + '-image]][' + subject + '-url]'
+  return link
+}
+
 exports.shields = function (subject, status, color) {
   var address = 'https://img.shields.io/badge/' + subject + '-' + status + '-' + color + '.svg?styles=flat-square'
   var link = '[' + subject + '-image]: '
-  return link + address
+  var url = '[' + subject + '-url]: '
+
+  return link + address + '\n' + url
+}
+
+exports.shields2 = function (subject, status, color, url) {
+  var address = 'https://img.shields.io/badge/' + subject + '-' + status + '-' + color + '.svg?styles=flat-square'
+  var link = '[![' + status + '](' + address + ')](' + url + ')'
+  return link
 }
